abdbb88b9b7dfd7b41f872f2064d604039d2b5a1
afu <afu>
2024-08-19 19:33:31 -0400

    tset

--- scripts/ecs_dev_status.sh
+++ scripts/ecs_dev_status.sh
@@ -5,6 +5,9 @@ ECS_CLUSTER=$IMAGE_REPO_NAME-$ENVIRONMENT
 ACCOUNTID=$(aws sts get-caller-identity --query 'Account' --output text)
 TASKNUM=$(cat z.auto.tfvars.json | jq -r .image | cut -f2 -d /)
 TASKDEF=arn:aws:ecs:us-east-1:$ACCOUNTID:task-definition/$TASKNUM
+
+echo $ACCOUNTID $TASKNUM $TASKDEF
+
 STATUS=$(aws ecs describe-services --cluster $ECS_CLUSTER --services $IMAGE_REPO_NAME | jq -r ".services[].deployments[] | select(.taskDefinition==\"$TASKDEF\").rolloutState")
 
 echo $ECS_CLUSTER
