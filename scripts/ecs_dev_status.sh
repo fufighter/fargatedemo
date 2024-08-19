16972f45cdf1311953d39def33e32d262a6c3de0
afu <afu>
2024-08-19 18:16:10 -0400

    test status

--- scripts/ecs_dev_status.sh
+++ scripts/ecs_dev_status.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#IMAGE_REPO_NAME="dog"
+#ENVIRONMENT="dev"
+
 ECS_CLUSTER=$IMAGE_REPO_NAME-$ENVIRONMENT
 STATUS=$(aws ecs describe-services --cluster $ECS_CLUSTER --services $IMAGE_REPO_NAME --no-cli-pager | jq -r '.services[].deployments[].rolloutState')
 
@@ -7,7 +10,7 @@ echo $STATUS
 
 while [ "${STATUS}" != "COMPLETED" ]
 do
-  echo "sleep 15... "
-  sleep 15
+  echo "${STATUS} sleep 30... "
+  sleep 30
   STATUS=$(aws ecs describe-services --cluster $ECS_CLUSTER --services $IMAGE_REPO_NAME --no-cli-pager | jq -r '.services[].deployments[].rolloutState')
 done
