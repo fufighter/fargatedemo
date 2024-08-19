e3db33a8b5107b664c2d0b06640e9a7b3e9a0451
afu <afu>
2024-08-19 19:12:43 -0400

    fix

--- scripts/ecs_dev_status.sh
+++ scripts/ecs_dev_status.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #IMAGE_REPO_NAME="dog"
 #ENVIRONMENT="dev"
-
 ECS_CLUSTER=$IMAGE_REPO_NAME-$ENVIRONMENT
 STATUS=$(aws ecs describe-services --cluster $ECS_CLUSTER --services $IMAGE_REPO_NAME | jq -r ".services[].deployments[] | select(.taskDefinition==\"$TASKARN\").rolloutState")
 TASKARN=$(cat z.auto.tfvars.json | jq -r .image)
@@ -13,5 +12,5 @@ while [ "$STATUS" != "COMPLETED" ]
 do
   sleep 30
   STATUS=$(aws ecs describe-services --cluster $ECS_CLUSTER --services $IMAGE_REPO_NAME | jq -r ".services[].deployments[] | select(.taskDefinition==\"$TASKARN\").rolloutState")
-  echo "wait 30... "
+  echo "$STATUS 30... "
 done
