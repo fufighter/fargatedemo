82c508937aca69a582ba754b144a328a618c0df6
afu <afu>
2024-08-21 12:21:15 -0400

    change path

--- src/main/java/com.example/HelloController.java
+++ src/main/java/com.example/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/dog")
     String hello() {
         return "Hello World, Spring Boot!";
     }
